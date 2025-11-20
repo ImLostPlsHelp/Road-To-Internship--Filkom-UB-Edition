8f4f4f2eed5e516f9c694fe0601f0d9975d51865
Raditya Rahmadhani <edogawaradit@gmail.com>
2025-11-20 21:53:29 +0700

    mengerjakan prolog

--- scripts/GlobalState.gd
+++ scripts/GlobalState.gd
@@ -6,11 +6,11 @@ var current_chapter = "prologue"
 
 # Fungsi praktis untuk pindah scene
 func go_to_scene(scene_path):
-    print("Pindah ke scene: ", scene_path)
-    # get_tree().change_scene_to_file(scene_path)
-    # Untuk latihan, kita beri komentar dulu agar tidak error
-    # Hapus komentar di atas jika Anda sudah punya scene berikutnya
+	print("Pindah ke scene: ", scene_path)
+	# get_tree().change_scene_to_file(scene_path)
+	# Untuk latihan, kita beri komentar dulu agar tidak error
+	# Hapus komentar di atas jika Anda sudah punya scene berikutnya
 
 # Fungsi untuk me-reset scene ini (jika gagal)
 func reload_current_scene():
-    get_tree().reload_current_scene()
+	get_tree().reload_current_scene()
